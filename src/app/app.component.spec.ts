9f50a6d4f7235ce589f240ffa2446754a128caf6
Raphael <raphael@akehir.com>
2020-11-10 10:14:46 +0100

    Update test script

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ShaderModule, ShaderService } from '@triangular/shader';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ShaderModule,
+      ],
+      providers: [
+        ShaderService,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
@@ -30,6 +35,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('web-marble-marcher app is running!');
+    expect(compiled.querySelector('main h1').textContent).toContain('web-marble-marcher');
   });
 });
